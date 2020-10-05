95a26d7f774907abafd7f0bee3d30d1299503de1
andres-karoll <andres.karoll@gmail.com>
2020-10-05 10:49:36 -0500

    rep

--- appausaws/src/main/java/co/edu/usbbog/appausa/appausaws/repository/IAfeccionMedicaRepository.java
+++
@@ -1,8 +0,0 @@
-package co.edu.usbbog.appausa.appausaws.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import co.edu.usbbog.appausa.appausaws.model.AfeccionMedica;
-
-public interface IAfeccionMedicaRepository extends JpaRepository<AfeccionMedica, String> {
-
-}
