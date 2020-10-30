862575b6b7eae2135b3d32af71122ca349d880ec
jdgonzalezd1 <60698448+jdgonzalezd1@users.noreply.github.com>
2020-10-30 10:24:05 -0500

    Delete AfeccionMedicaRepo.java

--- appausaws/repository/AfeccionMedicaRepo.java
+++
@@ -1,9 +0,0 @@
-package co.edu.usbbog.appausa.appausaws.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import co.edu.usbbog.appausa.appausaws.model.AfeccionMedica;
-
-public interface AfeccionMedicaRepo extends JpaRepository<AfeccionMedica,Integer>{
-
-}
