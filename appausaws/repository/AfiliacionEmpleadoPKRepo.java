b47869e5c884162ad3390e32ae13e39a99c86a3b
jdgonzalezd1 <60698448+jdgonzalezd1@users.noreply.github.com>
2020-10-30 10:20:28 -0500

    Delete AfiliacionEmpleadoPKRepo.java

--- appausaws/repository/AfiliacionEmpleadoPKRepo.java
+++
@@ -1,9 +0,0 @@
-package co.edu.usbbog.appausa.appausaws.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import co.edu.usbbog.appausa.appausaws.model.AfiliacionEmpleadoPK;
-
-public interface AfiliacionEmpleadoPKRepo extends JpaRepository<AfiliacionEmpleadoPK,Integer>{
-
-}
