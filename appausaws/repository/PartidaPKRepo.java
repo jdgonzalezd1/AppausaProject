975ba6fa9e06a20faa920243b81f3f95dedaeab2
jdgonzalezd1 <60698448+jdgonzalezd1@users.noreply.github.com>
2020-10-30 10:21:12 -0500

    Delete PartidaPKRepo.java

--- appausaws/repository/PartidaPKRepo.java
+++
@@ -1,9 +0,0 @@
-package co.edu.usbbog.appausa.appausaws.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import co.edu.usbbog.appausa.appausaws.model.PartidaPK;
-
-public interface PartidaPKRepo extends JpaRepository<PartidaPK,Integer> {
-
-}
